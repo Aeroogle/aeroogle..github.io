84125d8025d09722a2cf56dfb264c01f7415d960
Aeroogle <110894191+Aeroogle@users.noreply.github.com>
2024-07-30 11:51:13 +0800

    Add files via upload

---
+++ music/template/index.php
@@ -0,0 +1,138 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>音乐搜索</title>
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Cache-Control" content="no-transform">
+    <meta http-equiv="Cache-Control" content="no-siteapp">
+    <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="音乐搜索器">
+    <meta name="application-name" content="音乐搜索器">
+    <meta name="format-detection" content="telephone=no">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="static/img/apple-touch-icon.png">
+    <link rel="stylesheet" href="//cdn.staticfile.org/amazeui/2.3.0/css/amazeui.min.css">
+    <link rel="stylesheet" href="static/css/style.css?v<?php echo MC_VERSION; ?>">
+</head>
+<body>
+    <!--[if lte IE 9]>
+        <script type="text/javascript">
+            (function(){
+                var t = '你的浏览器也太挫了吧！大佬换一个噻！';
+                document.body.innerHTML = t;
+                document.body.style.fontSize = '66px';
+                document.body.style.textAlign = 'center';
+                document.body.style.background = '#000';
+                document.body.style.color = '#fff';
+                if (prompt('输入代号 666666 销毁此电脑: ', '') === '666666') {
+                    alert('拜拜了您呢~')
+                } else {
+                    alert('总感觉哪里不对');
+                }
+                window.open('', '_self', '');
+                window.close();
+            })();
+        </script>
+    <![endif]-->
+    <section class="am-g about">
+        <div class="am-container am-margin-vertical-xl">
+            <header class="am-padding-vertical">
+                <h2 class="about-title about-color">音乐搜索器</h2>
+            </header>
+            <hr>
+            <div class="am-u-lg-12 am-padding-vertical">
+                <form id="j-validator" class="am-form am-margin-bottom-lg" method="post">
+                    <div class="am-u-md-12 am-u-sm-centered">
+                        <ul id="j-nav" class="am-nav am-nav-pills am-nav-justify am-margin-bottom music-tabs">
+                            <li class="am-active" data-filter="name">
+                                <a>音乐名称</a>
+                            </li>
+                            <li data-filter="id">
+                                <a>音乐 ID</a>
+                            </li>
+                            <li data-filter="url">
+                                <a>音乐地址</a>
+                            </li>
+                        </ul>
+                        <div class="am-form-group">
+                            <input id="j-input" data-filter="name" class="am-form-field am-input-lg am-text-center am-radius" placeholder="例如: 微微 傅如乔" data-am-loading="{loadingText: ' '}" pattern="^.+$" required>
+                            <div class="am-alert am-alert-danger am-animation-shake"></div>
+                        </div>
+                        <div id="j-type" class="am-form-group am-text-center music-type">
+                        <?php foreach ($music_type_list as $key => $val) { ?>
+                            <label class="am-radio-inline">
+                                <input type="radio" name="music_type" value="<?php echo $key; ?>" data-am-ucheck<?php if ($key === 'netease') echo ' checked'; ?>>
+                                <?php echo $val; ?>
+                            </label>
+                            <?php if ($key === 'migu') echo '<br />'; ?>
+                        <?php } ?>
+                        </div>
+                        <button id="j-submit" type="submit" class="am-btn am-btn-primary am-btn-lg am-btn-block am-radius" data-am-loading="{spinner: 'cog', loadingText: '正在搜索相关音乐...', resetText: 'Get &#x221A;'}">Get &#x221A;</button>
+                    </div>
+                </form>
+                <form id="j-main" class="am-form am-u-md-12 am-u-sm-centered music-main">
+                    <a type="button" id="j-back" class="am-btn am-btn-success am-btn-lg am-btn-block am-radius am-margin-bottom-lg">&#x221A; 返回继续 <i class="am-icon-reply am-icon-fw"></i></a>
+                    <div class="am-g am-margin-bottom-sm">
+                        <div class="am-u-lg-6">
+                            <div class="am-input-group am-input-group-sm am-margin-bottom-sm" data-am-popover="{content: '音乐地址', trigger: 'hover'}">
+                                <span class="am-input-group-label"><i class="am-icon-link am-icon-fw"></i></span>
+                                <input id="j-link" class="am-form-field" readonly>
+                                <span class="am-input-group-btn">
+                                    <a id="j-link-btn" class="am-btn am-btn-default" target="_blank">
+                                        <i class="am-icon-external-link"></i>
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="am-u-lg-6">
+                            <div class="am-input-group am-input-group-sm am-margin-bottom-sm" data-am-popover="{content: '音乐链接', trigger: 'hover'}">
+                                <span class="am-input-group-label"><i class="am-icon-music am-icon-fw"></i></span>
+                                <input id="j-src" class="am-form-field" readonly>
+                                <span class="am-input-group-btn">
+                                    <a id="j-src-btn" class="am-btn am-btn-default" target="_blank">
+                                        <i id="j-src-btn-icon" class="am-icon-external-link"></i>
+                                    </a>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="am-g">
+                    </div>
+                    <div class="am-g">
+                        <div class="am-u-lg-6">
+                            <div class="am-input-group am-input-group-sm am-margin-bottom-sm" data-am-popover="{}">
+                            </div>
+                        </div>
+                        </div>
+                    <div id="j-show" class="am-margin-vertical">
+                        <div id="j-player" class="aplayer"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="am-popup" id="copr-info">
+            <div class="am-popup-inner">
+                <div class="am-popup-hd">
+                    <span data-am-modal-close class="am-close">&times;</span>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <footer class="footer">
+    </footer>
+    <script src="//cdn.staticfile.org/jquery/1.12.4/jquery.min.js"></script>
+    <script src="//cdn.staticfile.org/amazeui/2.3.0/js/amazeui.min.js"></script>
+    <script src="//cdn.staticfile.org/aplayer/1.6.0/APlayer.min.js"></script>
+    <script src="//cdn.staticfile.org/Base64/1.0.1/base64.min.js"></script>
+    <script src="static/js/music.js?v<?php echo MC_VERSION; ?>"></script>
+    <script language="javascript" type="text/javascript" src="http://js.users.51.la/18759442.js"></script>
+<div style="display: none">
+<script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1279824854&web_id=1279824854"></script>
+    </div>
+</body>
+</html>
